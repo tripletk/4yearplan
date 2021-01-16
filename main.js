5525efc548a0ca3be98ab7ea2df5b94dd03df16b
mjk9913 <75086132+mjk9913@users.noreply.github.com>
2021-01-16 22:09:29 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function moveSignup() {
+    document.querySelector(".modalBG").style.display = "flex";
+}
+
+function xSignup() {
+    document.querySelector(".modalBG").style.display = "none";
+}
+
+function moveSignin() {
+    document.querySelector(".signBG").style.display = "flex";
+}
+
+function xSignin() {
+    document.querySelector(".signBG").style.display = "none";
+}
