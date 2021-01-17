a5b2b349e7ab32199e8626e50176a33b53a13e89
Kevin L <chenkuanlai@gmail.com>
2021-01-16 21:03:26 -0800

    Wow.

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ class course {
     }
 }
 
-let seasons = document.getElementsByClassName("season");
+let years = document.getElementsByClassName("year");
+    terms = ["fall", "winter", "spring"];
     courses = ["CS 1", "CS 2", "CS 3", "CS 4", "MATH 1", "MATH 2", "MATH 3", "MATH 4", "PHYS 1", "PHYS 2", "PHYS 3", "PHYS 4"];
     plan = [["!", "!", "!", "!"], 
             ["!", "!", "!", "!"], 
@@ -30,16 +31,38 @@ for (let i = 0; i < courses.length; i++) {
     courses[i] = new course(courses[i], 4);
 }
 
-for (let i = 0; i < seasons.length; i++) {
-    for (let j = 0; j < 4; j++) {
-        let newdropzone = document.createElement("div");
-        let newdropstate = document.createElement("div");
-        newdropzone.className = "dropzone";
-        newdropzone.id = i + "-" + j;
-        newdropstate.className = "dropstate";
-        newdropstate.innerHTML = "placeholder";
-        newdropzone.appendChild(newdropstate);
-        seasons[i].appendChild(newdropzone);
+for (let i = 0; i < years.length; i++) {
+    let newyearlabel = document.createElement("div");
+    newyearlabel.className = "yearlabel";
+    newyearlabel.innerHTML = "year " + (i + 1);
+    years[i].appendChild(newyearlabel);
+    for (let j = 0; j < terms.length; j++) {
+        let newtermtab = document.createElement("div");
+        let newtermlabel = document.createElement("div");
+        let newtermclear = document.createElement("p");
+        let newterm = document.createElement("div");
+        newtermtab.className = "termtab";
+        newtermlabel.className = "termlabel";
+        newterm.className = "term";
+        newtermlabel.innerHTML = terms[j];
+        newtermclear.innerHTML = "clear courses for " + terms[j];
+        newtermclear.style.fontWeight = "500";
+        newtermclear.addEventListener("mouseover", grow);
+        newtermclear.addEventListener("mouseleave", ungrow);
+        newtermclear.addEventListener("click", clearterm);
+        for (let k = 0; k < 4; k++) {
+            let newdropzone = document.createElement("div");
+            let newdropstate = document.createElement("div");
+            newdropzone.className = "dropzone";
+            newdropstate.className = "dropstate";
+            newdropstate.innerHTML = "placeholder";
+            newdropzone.appendChild(newdropstate);
+            newterm.appendChild(newdropzone);
+        }
+        newtermtab.appendChild(newtermlabel);
+        newtermtab.appendChild(newtermclear);
+        years[i].appendChild(newtermtab);
+        years[i].appendChild(newterm);
     }
 }
 
@@ -94,4 +117,16 @@ function debugplan() {
         row += "R" + (i + 1);
         console.log(row);
     }
+}
+
+function grow() {
+    this.style.transform = "scale(1.1)";
+}
+
+function ungrow() {
+    this.style.transform = "scale(1)";
+}
+
+function clearterm() {
+
 }
