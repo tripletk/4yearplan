c67c69a427d11ae915249086baafbca6874ed3b5
Kaung Khant Kyaw <39270802+tripletk@users.noreply.github.com>
2021-01-16 17:15:29 -0800

    added dev comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+
+// Note: The production code is in static/js. I will take code from here and merge it into prod.  
+
+
+
 function openWindow(divName) {
     document.querySelector(divName).style.display = "flex";
 }
